71604a67eaadc2247018cc879f883bcc1d88cb43
Jeddy Certifico <jeddycolon.certifico@bicol-u.edu.ph>
2023-05-10 14:09:41 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+require "autoload.php";
+
+$uri = $_SERVER["REQUEST_URI"];
+
+switch ($uri) {
+    case "/":
+        $title = "Home";
+        require "views/home.php";
+        break;
+    case "/about":
+        $title = "About";
+        require "views/about.php";
+        break;
+    case "/services":
+        $title = "Services";
+        require "views/services.php";
+        break;
+    case "/works":
+        $title = "Works";
+        require "views/works.php";
+        break;
+    case "/contact":
+        $title = "Contact";
+        require "views/contact.php";
+        break;
+    default:
+        $title = "404";
+        require "views/404.php";
+        break;
+}
+
+// require "views/index.php";
---
+++ views/partials/footer.php
@@ -0,0 +1,13 @@
+<!--FOOTER-->
+<footer class="footer conatiner-fluid">
+    <p class="footer__title">Tayam_Certifico</p>
+    <div class="footer__social">
+        <a href="#" class="footer__icon"><i class="bx bxl-facebook"></i></a>
+        <a href="#" class="footer__icon"><i class="bx bxl-instagram"></i></a>
+        <a href="#" class="footer__icon"><i class="bx bxl-twitter"></i></a>
+    </div>
+    <p>&#169; 2023 copyright all right reserved</p>
+</footer>
+</body>
+
+</html>
