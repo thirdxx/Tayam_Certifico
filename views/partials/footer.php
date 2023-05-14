7c2a1a46efe1c80687e67360f8d82141f0cf7ad1
Jeddy Certifico <jeddycolon.certifico@bicol-u.edu.ph>
2023-05-15 01:49:15 +0800

    css changes

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,13 +1,20 @@
-<!--FOOTER-->
-<footer class="footer conatiner-fluid">
-    <p class="footer__title">Tayam_Certifico</p>
-    <div class="footer__social">
-        <a href="#" class="footer__icon"><i class="bx bxl-facebook"></i></a>
-        <a href="#" class="footer__icon"><i class="bx bxl-instagram"></i></a>
-        <a href="#" class="footer__icon"><i class="bx bxl-twitter"></i></a>
-    </div>
-    <p>&#169; 2023 copyright all right reserved</p>
-</footer>
-</body>
-
+   <!-- FOOTER -->
+   <footer class="footer conatiner-fluid">
+      <p class="footer__title">Tayam_Certifico</p>
+      <div class="footer__social">
+        <a href="https://www.facebook.com/bunso.tayam" class="footer__icon"
+          ><i class="bx bxl-facebook"></i
+        ></a>
+       <a href="index.html" type="solid" class="footer__icon"
+          ><i class="bx bxs-home"></i
+        ></a>
+        <a
+          href="https://github.com/thirdxx/Tayam_Certifico"
+          class="footer__icon"
+          ><i class="bx bxl-github"></i
+        ></a>
+      </div>
+      <p class="copyright">&#169; 2023 copyright all rights reserved</p>
+    </footer>
+  </body>
 </html>
