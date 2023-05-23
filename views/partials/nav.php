5f24cc76e6d1a924300170b39b330f21e8a847a5
thirdxx <diogenesiiiaraojo.tayam@bicol-u.edu.ph>
2023-05-23 13:13:24 +0800

    add dropdown menu

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,3 +1,41 @@
+<style>
+    .dropdown-menu {
+        display: none;
+        position: absolute;
+        background-color: #f9f9f9;
+        text-align: center;
+        min-width: 160px;
+        box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+        padding: 10px;
+        z-index: 1;
+        left: 0;
+
+    }
+
+    .dropdown-menu li {
+        list-style-type: none;
+        margin: 5px 0;
+    }
+
+    .dropdown-menu li a {
+        text-decoration: none;
+        color: #333;
+    }
+
+    .dropdown-menu li a:hover {
+        background: linear-gradient(to right, blue, #00ff00);
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        color: #fff;
+    }
+
+    .nav__toggle:hover .dropdown-menu {
+        display: block;
+        left: 100%;
+        transform: translateX(-100%);
+    }
+</style>
 <nav class="nav bd-grid">
     <div>
         <a href="./" class="nav__logo text-decoration-none">Portfolio</a>
@@ -28,6 +66,13 @@
 
     <div class="nav__toggle" id="nav-toggle">
         <i class="bx bx-menu"></i>
+        <ul class="dropdown-menu">
+            <li><a href="./">Home</a></li>
+            <li><a href="./about">About</a></li>
+            <li><a href="./services">Services</a></li>
+            <li><a href="./works">Works</a></li>
+            <li><a href="./contact">Contact</a></li>
+        </ul>
     </div>
 </nav>
 
