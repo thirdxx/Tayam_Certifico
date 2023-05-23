4d2f37b23b565c4bd10f3c863331f63f98f08802
JeddyCertifico <jeddycolon.certifico@bicol-u.edu.ph>
2023-05-23 12:38:04 +0800

    fix sessions

--- views/services.php
+++ views/services.php
@@ -1,54 +1,61 @@
 <?php
 $css = "services.css";
 require "partials/header.php";
+
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 ?>
 
-<!-- HEADER -->
-<header class="l-header">
-  <?php require "partials/nav.php"; ?>
-</header>
+  <!-- HEADER -->
+  <header class="l-header">
+    <?php require "partials/nav.php"; ?>
+  </header>
 
-<div id="card">
-  <h1>Services</h1>
-  <div class="image-crop">
-    <img id="avatar" src="./resources/img/diogenespic.png" alt="Diogenes Tayam"></img>
-  </div>
-  <div id="bio">
-    <div id="texts">
-      <p class="text"> Photography<br>
-        I am sufficient in photography. I capture stunning portraits and also loves to captures the beauty of the nature.<br><br>
-      <p class="text"> Layouting<br>
-        I am knowledgeable in layouting. I create your imagination into reality.<br><br>
-      <p class="text"> Photo & Video Editing<br>
-        Photo & video editing is my passion. I make photos and videos more stunning to the eyes and creative.<br><br>
-      <p class="text"> Front-end Web Development<br>
-        I am also sufficient in front-end web development.<br><br>
-      </p>
-      </p>
-      </p>
-      </p>
-      </p>
-      <div id="buttons">
-        <button onclick="location.href = './works';" class="button1">My works</button>
+  <div id="card">
+    <h1>Services</h1>
+    <div class="image-crop">
+      <img id="avatar" src="./resources/img/diogenespic.png" alt="Diogenes Tayam"></img>
+    </div>
+    <div id="bio">
+      <div id="texts">
+        <p class="text"> Photography<br>
+          I am sufficient in photography. I capture stunning portraits and also loves to captures the beauty of the nature.<br><br>
+        <p class="text"> Layouting<br>
+          I am knowledgeable in layouting. I create your imagination into reality.<br><br>
+        <p class="text"> Photo & Video Editing<br>
+          Photo & video editing is my passion. I make photos and videos more stunning to the eyes and creative.<br><br>
+        <p class="text"> Front-end Web Development<br>
+          I am also sufficient in front-end web development.<br><br>
+        </p>
+        </p>
+        </p>
+        </p>
+        </p>
+        <div id="buttons">
+          <button onclick="location.href = './works';" class="button1">My works</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
-<div id="card2">
-  <h2>Services</h2>
-  <div class="image-crop2">
-    <img id="avatar" src="./resources/img/jeddypic.png" alt="Jeddy Certifico"></img>
-  </div>
-  <div id="bio">
-    <p class="text2">Web Development<br>
-      I am a sufficient in various programming languages such as JavaScript, PHP, HTML, CSS, and also I am knowledgable in C, Python, and Java prhramming languages.<br><br>
-    </p>
-    <div id="buttons">
-      <button onclick="location.href = './works';" class="button2">My works</button>
+  <div id="card2">
+    <h2>Services</h2>
+    <div class="image-crop2">
+      <img id="avatar" src="./resources/img/jeddypic.png" alt="Jeddy Certifico"></img>
+    </div>
+    <div id="bio">
+      <p class="text2">Web Development<br>
+        I am a sufficient in various programming languages such as JavaScript, PHP, HTML, CSS, and also I am knowledgable in C, Python, and Java prhramming languages.<br><br>
+      </p>
+      <div id="buttons">
+        <button onclick="location.href = './works';" class="button2">My works</button>
+      </div>
     </div>
   </div>
-</div>
-</div>
-</main>
+  </div>
+  </main>
 
-<?php require "partials/footer.php"; ?>
+  <?php require "partials/footer.php"; ?>
+
+<?php
+} else {
+  header("Location: ./login");
+}
--- views/works.php
+++ views/works.php
@@ -1,66 +1,74 @@
 <?php
 $css = "works.css";
 require "partials/header.php";
+
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 ?>
 
-<!-- HEADER -->
-<header class="l-header">
-  <?php require "partials/nav.php"; ?>
-</header>
-<main>
+  <!-- HEADER -->
+  <header class="l-header">
+    <?php require "partials/nav.php"; ?>
+  </header>
+  <main>
 
-  <!-- WORKS -->
-  <section class="work section" id="work">
-    <h2 class="section-title">Diogenes Tayam</h2>
+    <!-- WORKS -->
+    <section class="work section" id="work">
+      <h2 class="section-title">Diogenes Tayam</h2>
 
-    <div class="work__container bd-grid">
-      <div class="work__img">
-        <img src="./resources/img/pic1.jpg" alt="" />
+      <div class="work__container bd-grid">
+        <div class="work__img">
+          <img src="./resources/img/pic1.jpg" alt="" />
+        </div>
+        <div class="work__img">
+          <img src="./resources/img/pic7.jpeg" alt="" />
+        </div>
+        <div class="work__img">
+          <img src="./resources/img/pic8.jpeg" alt="" />
+        </div>
+        <div class="work__img">
+          <img src="./resources/img/pic10.jpg" alt="" />
+        </div>
+        <div class="work__img">
+          <img src="./resources/img/pic9.jpeg" alt="" />
+        </div>
+        <div class="work__img">
+          <img src="./resources/img/pic11.jpg" alt="" />
+        </div>
       </div>
-      <div class="work__img">
-        <img src="./resources/img/pic7.jpeg" alt="" />
-      </div>
-      <div class="work__img">
-        <img src="./resources/img/pic8.jpeg" alt="" />
-      </div>
-      <div class="work__img">
-        <img src="./resources/img/pic10.jpg" alt="" />
-      </div>
-      <div class="work__img">
-        <img src="./resources/img/pic9.jpeg" alt="" />
-      </div>
-      <div class="work__img">
-        <img src="./resources/img/pic11.jpg" alt="" />
-      </div>
-    </div>
-  </section>
-  <section class="work section" id="work">
-    <h3 class="section-title2">Jeddy Certifico</h3>
+    </section>
+    <section class="work section" id="work">
+      <h3 class="section-title2">Jeddy Certifico</h3>
 
-    <div class="work__container bd-grid">
-      <div class="work__img">
-        <img src="./resources/img/jed1.png" alt="" />
+      <div class="work__container bd-grid">
+        <div class="work__img">
+          <img src="./resources/img/jed1.png" alt="" />
+        </div>
+        <div class="work__img">
+          <img src="./resources/img/jed2.png" alt="" />
+        </div>
+        <div class="work__img">
+          <img src="./resources/img/jed3.png" alt="" />
+        </div>
+        <div class="work__img">
+          <img src="./resources/img/jed5.png" alt="" />
+        </div>
+        <div class="work__img">
+          <img src="./resources/img/jed6.png" alt="" />
+        </div>
+        <div class="work__img">
+          <img src="./resources/img/jeddyc.png" alt="" />
+        </div>
       </div>
-      <div class="work__img">
-        <img src="./resources/img/jed2.png" alt="" />
+      <div id="buttons">
+        <button onclick="location.href = './services';" class="button1">Back</button>
       </div>
-      <div class="work__img">
-        <img src="./resources/img/jed3.png" alt="" />
-      </div>
-      <div class="work__img">
-        <img src="./resources/img/jed5.png" alt="" />
-      </div>
-      <div class="work__img">
-        <img src="./resources/img/jed6.png" alt="" />
-      </div>
-      <div class="work__img">
-        <img src="./resources/img/jeddyc.png" alt="" />
-      </div>
-    </div>
-    <div id="buttons">
-      <button onclick="location.href = './services';" class="button1">Back</button>
-    </div>
-  </section>
-</main>
+    </section>
+  </main>
 
-<?php require "partials/footer.php"; ?>
+  <?php require "partials/footer.php"; ?>
+
+<?php
+} else {
+  header("Location: ./login");
+  exit();
+}
