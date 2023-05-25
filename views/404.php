d2a4a71055efdb1c99cc919315dabe2a0fda5031
JeddyCertifico <jeddycolon.certifico@bicol-u.edu.ph>
2023-05-25 15:58:37 +0800

    404 UI

--- views/404.php
+++ views/404.php
@@ -1,1 +1,18 @@
-<h1>Page not Found!</h1>
+<?php
+session_start();
+$css = "styles.css";
+require "partials/header.php";
+?>
+<!-- HEADER -->
+<header class="l-header">
+    <?php require "partials/nav.php"; ?>
+</header>
+
+<div class="container d-grid" style="min-height: 100vh; place-items:center">
+    <div class=" text-center">
+        <h1 class="fw-bolder">404</h1>
+        <h1 class="fw-bold">Page not found.</h1>
+    </div>
+</div>
+
+<?php require "partials/footer.php"; ?>
